2a9626deedf215cbec3b82f6d466542a821804ad
Mia2013 <komlosnikolett@gmail.com>
2023-01-10 12:58:26 +0100

    add routes, add home and content page, add navbar component to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Content from "./pages/Content";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="content" element={<Content />} />
+      </Routes>
     </div>
   );
 }
