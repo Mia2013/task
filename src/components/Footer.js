8bc32ab64427dd4a786eea932e8fed71a3b1b9dd
Mia2013 <komlosnikolett@gmail.com>
2023-01-13 12:13:27 +0100

    remove loop:true from Footer Typeit component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,17 @@ export default function Footer() {
     <div id="footer">
       <Container className="d-flex justify-content-center align-items-center ">
         <p className="fst-italic fw-lighter my-1">
-          Készítette:{" "}
-          <TypeIt options={{ loop: true }}>Budavári Nikolett</TypeIt>
+          {" "}
+          <TypeIt
+            options={{
+              speed: 250,
+              afterComplete: function (instance) {
+                instance.destroy();
+              },
+            }}
+          >
+            Készítette: Budavári Nikolett
+          </TypeIt>
         </p>
       </Container>
     </div>
